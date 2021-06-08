6a0d292d62e65758b725f60b336735550dd56b8f
Rikktee <Tuitjer@technologiestiftung-berlin.de>
2021-06-08 12:32:46 +0200

    First try interior

---
+++ tiles/decoration/food_and_drinks.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="food_and_drinks" tilewidth="32" tileheight="32" tilecount="144" columns="16">
+ <image source="decoration/food_and_drinks.png" width="512" height="300"/>
+ <tile id="1">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
